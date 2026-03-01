814ec223b0f0f741d2a7c4010a239fefc774e170
darius-it <179517889+darius-it@users.noreply.github.com>
2026-03-01 12:57:03 +0100

    Revert ktor version variable change

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ repositories {
     mavenCentral()
 }
 
-val ktorVersion: String by project
+val ktor_version: String by project
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("io.ktor:ktor-client-core:${ktorVersion}")
-    implementation("io.ktor:ktor-client-cio:${ktorVersion}")
-    testImplementation("io.ktor:ktor-client-mock:${ktorVersion}")
+    implementation("io.ktor:ktor-client-core:${ktor_version}")
+    implementation("io.ktor:ktor-client-cio:${ktor_version}")
+    testImplementation("io.ktor:ktor-client-mock:${ktor_version}")
     implementation("org.slf4j:slf4j-simple:2.0.3")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 
