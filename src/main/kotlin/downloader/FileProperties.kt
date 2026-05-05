e5064b953c2a1c51083d0b3e08e046813b6ffb0c
Darius <179517889+darius-it@users.noreply.github.com>
2026-05-05 12:20:22 +0200

    Add custom exception for wrong status code in fetchFileProperties

--- src/main/kotlin/downloader/FileProperties.kt
+++ src/main/kotlin/downloader/FileProperties.kt
@@ -2,23 +2,21 @@ package me.dariusit.downloader
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.head
+import io.ktor.http.HttpStatusCode
+
+class FilePropertiesWrongStatusCodeException(statusCode: HttpStatusCode) :
+    Exception("Failed to get file information via HEAD request (received unexpected HTTP status code $statusCode)!")
 
 data class FileProperties (val contentLength: Int, val acceptRanges: String? = null) {
     // TODO: see if we can easily pull checksum from file server without downloading entire file
     // Idea: add checksum files like for Linux ISOs and implement optional verification at the end of a download if they exist
 
     companion object {
-        suspend fun fetchFileProperties(httpClient: HttpClient, fileName: String): FileProperties? {
+        suspend fun fetchFileProperties(httpClient: HttpClient, fileName: String): FileProperties {
             val response = httpClient.head(fileName)
 
-            // TODO: add custom exception
-
-            if (response.status != io.ktor.http.HttpStatusCode.OK) {
-                println("Something went wrong when getting the HEAD request! Status code: ${response.status}")
-
-                // potentially do some better error handling here
-                // TODO: add custom exception here, something like UnexpectedStatusCodeException
-                return null
+            if (response.status != HttpStatusCode.OK) {
+                throw FilePropertiesWrongStatusCodeException(response.status)
             }
 
             return FileProperties(
